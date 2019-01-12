367ad10d7b1b81594b90cfdbe830285e1357e8b4
Manas Hejmadi <manashejmadi@gmail.com>
2019-01-13 02:31:45 +0530

    Untested - BigChainDB Transfer Functionality
    Added Transfer Functionality to the BigChain Driver Class

--- Dendrite/Dendrite/bigchainuploader.py
+++ Dendrite/Dendrite/bigchainuploader.py
@@ -66,7 +66,8 @@ class BigChainUploader:
             prepared_block,
             private_keys=self.keypairs.private_key
         )
-        self.fulfilled_block_id = f['id']
+        self.fulfilled_block = f
+        self.fulfilled_outputs = f['outputs'][0]
         return f
 
 
@@ -74,7 +75,7 @@ class BigChainUploader:
         meta = self.metadata
         metadata = {}
         for data in meta:
-            metadata[data['DEPARTMENT']] = [data['METADATA'], data['TIMESTAMP']]
+            metadata[data['DEPARTMENT']] = [data['METADATA'], data['TIMESTAMP'], data['ROLE']]
         return metadata
 
     def prepare_genesis_transaction(self):
@@ -87,6 +88,7 @@ class BigChainUploader:
 
     def stage_metadata(self, metadata, sender):
         meta = {
+            'ROLE': current_user.role,
             'DEPARTMENT': sender,
             'METADATA': metadata,
             'TIMESTAMP': self.timestamp()
@@ -101,6 +103,47 @@ class BigChainUploader:
                     self.prepare_genesis_transaction()
                 )
             )
-            return {'Success': True, 'block_id': self.fulfilled_block_id}
+            return {'Success': True, 'block': self.fulfilled_block, 'output':self.fulfilled_outputs}
         except Exception as e:
             return {'Success': False, 'Exception': e}
+
+    def getTransferInput(self):
+        transfer_input = {
+            'fulfills': {
+                'transaction_id': self.prev_fulfilled_block['id'],
+                'output_index': 0
+            },
+            'owners_before': [self.owner.public_key],
+            'fulfillment': self.prev_block_output['condition']['details']
+        }
+        return transfer_input
+
+    def UploadTransferData(self, prev_fulfilled_block, prev_block_output, owner, recipient):
+        self.prev_fulfilled_block = prev_fulfilled_block
+        self.prev_block_output = prev_block_output
+        self.owner = owner
+        self.recipient = recipient
+
+    def TransferBlock(self):
+        # Prepare
+        prepared_transfer_tx = bdb.transactions.prepare(
+            operation='TRANSFER',
+            asset={'id': self.prev_fulfilled_block['id']},
+            metadata=self.get_metadata(),
+            inputs=self.getTransferInput(),
+            recipients=self.recipient.public_key
+        )
+        # Fullfill Transactions
+        fulfilled_transfer_tx = bdb.transactions.fulfill(
+            prepared_transfer_tx,
+            private_keys=self.owner.private_key
+        )
+        # Change the previous block data to current block to accomodate next block
+        self.prev_fulfilled_block = fulfilled_transfer_tx
+        self.prev_block_output = fulfilled_transfer_tx['outputs'][0]
+        # Send Transaction
+        bdb.transactions.send_commit(fulfilled_transfer_tx)
+        return {'Success': True, 'block': self.prev_fulfilled_block, 'output':self.prev_block_output}
+
+
+
